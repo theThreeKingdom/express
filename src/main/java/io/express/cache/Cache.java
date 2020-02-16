9f3252a5ff4adf0cd3dd89f5dcbb452b6c87bbe2
nixin <linginfanta@163.com>
2020-02-16 19:15:12 +0800

    framework init

--- src/main/java/io/express/cache/Cache.java
+++ src/main/java/io/express/cache/Cache.java
@@ -2,13 +2,18 @@ package io.express.cache;
 
 import java.util.List;
 
-public interface Cache
-{
-	public Object get(Object key) throws CacheException;
-	public void put(Object key, Object value) throws CacheException;
-	public void update(Object key, Object value) throws CacheException;
-	public List<Object> keys() throws CacheException ;
-	public void remove(Object key) throws CacheException;
-	public void clear() throws CacheException;
-	public void destroy() throws CacheException;
+public interface Cache {
+    public Object get(Object key) throws CacheException;
+
+    public void put(Object key, Object value) throws CacheException;
+
+    public void update(Object key, Object value) throws CacheException;
+
+    public List<Object> keys() throws CacheException;
+
+    public void remove(Object key) throws CacheException;
+
+    public void clear() throws CacheException;
+
+    public void destroy() throws CacheException;
 }
