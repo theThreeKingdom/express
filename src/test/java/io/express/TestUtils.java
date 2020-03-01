17062983fb40d24b72a326a6aa4055cda7950dcb
nixin <linginfanta@163.com>
2020-03-01 16:22:21 +0800

    add config file

--- src/test/java/io/express/TestUtils.java
+++ src/test/java/io/express/TestUtils.java
@@ -14,11 +14,6 @@ import static org.junit.Assert.assertEquals;
 public class TestUtils {
 
     @Test
-    public void testGetRandom(){
-        assertEquals(888,Utils.getRandom(3));
-    }
-
-    @Test
     public void testGetMd5Sum(){
         assertEquals("e10adc3949ba59abbe56e057f20f883e",Utils.getMd5Sum("123456"));
     }
