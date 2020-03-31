0f7977a52aa378af8277839cc13e9d88e08bf55a
nixin <linginfanta@163.com>
2020-03-31 12:14:54 +0800

    add templateRenderer sample

--- src/main/java/com/sample/Sample.java
+++ src/main/java/com/sample/Sample.java
@@ -1,11 +1,16 @@
 package com.sample;
 
+import io.express.ActionContext;
 import io.express.Mapping;
 import io.express.renderer.Renderer;
 import io.express.renderer.TemplateRenderer;
 import io.express.renderer.TextRenderer;
+import io.express.template.Template;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @ClassName Sample
@@ -20,5 +25,28 @@ public class Sample {
     @Mapping("/")
     public Renderer home() {
         return new TextRenderer("Hello world");
+    }
+
+    @Mapping("/test.html")
+    public Renderer test(){
+        Map<String,Object> model = new HashMap<String,Object>();
+        try{
+            String client_ip = ActionContext.getActionContext().getHttpServletRequest().getHeader("X-Real-IP");
+
+            if(client_ip == null || client_ip.length() == 0 || "unknown".equalsIgnoreCase(client_ip))
+                client_ip = ActionContext.getActionContext().getHttpServletRequest().getHeader("x-forwarded-for");
+            if(client_ip == null || client_ip.length() == 0 || "unknown".equalsIgnoreCase(client_ip))
+                client_ip = ActionContext.getActionContext().getHttpServletRequest().getHeader("Proxy-Client-IP");
+            if(client_ip == null || client_ip.length() == 0 || "unknown".equalsIgnoreCase(client_ip))
+                client_ip = ActionContext.getActionContext().getHttpServletRequest().getHeader("WL-Proxy-Client-IP");
+            if(client_ip == null || client_ip.length() == 0 || "unknown".equalsIgnoreCase(client_ip))
+                client_ip = ActionContext.getActionContext().getHttpServletRequest().getRemoteAddr();
+
+            log.info("client ip = " +client_ip);
+            model.put("client",client_ip);
+        }catch (Exception e){
+
+        }
+        return new TemplateRenderer("/test.html","model",model);
     }
 }
