b84b616e5b1e47441254a67b8f5f7854c9ab325d
nixin <linginfanta@163.com>
2021-12-31 11:59:24 +0800

    add Thymeleaf template support,and implement function

--- src/main/java/io/express/template/ThymeleafTemplate.java
+++ src/main/java/io/express/template/ThymeleafTemplate.java
@@ -1,5 +1,8 @@
 package io.express.template;
 
+import org.thymeleaf.context.EngineContext;
+import org.thymeleaf.context.Context;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
@@ -13,8 +16,22 @@ import java.util.Map;
  **/
 public class ThymeleafTemplate implements Template {
 
+    private org.thymeleaf.templateresource.ITemplateResource template;
+    private String contentType;
+    private String encoding;
+
+    public ThymeleafTemplate(org.thymeleaf.templateresource.ITemplateResource template,String contentType,String encoding){
+        this.template = template;
+        this.contentType = contentType;
+        this.encoding = encoding;
+    }
     @Override
     public void render(HttpServletRequest request, HttpServletResponse response, Map<String, Object> model) throws Exception {
+        StringBuilder sb = new StringBuilder(64);
+        sb.append(contentType == null ? "text/html" : contentType).append(";charset=").append(encoding == null ? "UTF-8" : encoding);
+        response.setContentType(sb.toString());
+        response.setCharacterEncoding(encoding == null ? "UTF-8" : encoding);
+        // init context:
 
     }
 }
--- src/main/java/io/express/template/ThymeleafTemplateFactory.java
+++ src/main/java/io/express/template/ThymeleafTemplateFactory.java
@@ -4,6 +4,8 @@ import io.express.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+
 /**
  * @ClassName ThymeleafTemplateFactory
  * @Description TODO
@@ -14,9 +16,22 @@ import org.slf4j.LoggerFactory;
 public class ThymeleafTemplateFactory extends TemplateFactory {
     private final Logger log = LoggerFactory.getLogger(getClass());
 
+
+    private String inputEncoding = "UTF-8";
+    private String outputEncoding = "UTF-8";
+
     @Override
     public void init(Config config) {
-
+        String webAppPath = config.getServletContext().getRealPath("/");
+        if (webAppPath == null) {
+            String err = "Cannot get web application path. Are you deploy the application as a .war file?";
+            log.warn(err);
+            throw new ExceptionInInitializerError(err);
+        }
+        if (!webAppPath.endsWith("/") && !webAppPath.endsWith("\\"))
+            webAppPath = webAppPath + File.separator;
+        log.info("Detect web application path: " + webAppPath);
+        log.info("init ThymeleafTemplateFactory...");
     }
 
     @Override
